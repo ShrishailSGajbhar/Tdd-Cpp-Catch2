ed206f5a8b161f230d655d321ee5191cb0f1280e
Shrishail <shrishailgajbhar5@gmail.com>
2020-11-02 23:53:11 +0530

    Add Tdd using Cpp and Catch2

---
+++ tests/test_tdd_fizzbuzz.cpp
@@ -0,0 +1,42 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_session.hpp>
+#include <string>
+#include "fizzbuzz.h"
+
+int main( int argc, char* argv[] ) {
+  // global setup...
+
+  int result = Catch::Session().run( argc, argv );
+
+  // global clean-up...
+
+  return result;
+}
+
+TEST_CASE("For input as 0 output should be FizzBuzz") {
+  REQUIRE(fizzBuzz(0) == "FizzBuzz");
+}
+
+TEST_CASE("For input as 1 output should be string 1") {
+  REQUIRE(fizzBuzz(1) == "1");
+}
+
+TEST_CASE("For input as 3 output should be Fizz") {
+  REQUIRE(fizzBuzz(3) == "Fizz");
+}
+
+TEST_CASE("For input as 5 output should be Buzz") {
+  REQUIRE(fizzBuzz(5) == "Buzz");
+}
+
+TEST_CASE("For input as multiple of 3 output should be Fizz") {
+  REQUIRE(fizzBuzz(6) == "Fizz");
+}
+
+TEST_CASE("For input as multiple of 5 output should be Buzz") {
+  REQUIRE(fizzBuzz(10) == "Buzz");
+}
+
+TEST_CASE("For input multiple of both 3 & 5 output should be FizzBuzz") {
+  REQUIRE(fizzBuzz(15) == "FizzBuzz");
+}
